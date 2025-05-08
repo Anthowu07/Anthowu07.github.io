b530f9c1ba94ab398d9c8c223418f2ab45e8221c
Anthowu07 <anthony07wu@gmail.com>
2025-05-08 03:03:37 -0400

    debug

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Navbar.css';
-import train from '../assets/train.png'
+import train from '../assets/train.png';
 
-export default function Navbar(){
-    return (
-        <nav className="nav">
-            <a href="/" className="site-title">All a Board</a>
-            <img src={train} alt="train"/>
-            <ul>
-                <li><a href="/boardgames">Board Games</a></li>
-                <li><a href="/warehouses">Warehouses</a></li>
-                <li><a href="/inventory">Inventory</a></li>
-                <li><a href="/orders">Place Order</a></li>
-            </ul>
-        </nav>
-    )
+export default function Navbar() {
+  return (
+    <nav className="nav">
+      <Link to="/" className="site-title">All a Board</Link>
+      <img src={train} alt="train" />
+      <ul>
+        <li><Link to="/boardgames">Board Games</Link></li>
+        <li><Link to="/warehouses">Warehouses</Link></li>
+        <li><Link to="/inventory">Inventory</Link></li>
+        <li><Link to="/orders">Place Order</Link></li>
+      </ul>
+    </nav>
+  );
 }
